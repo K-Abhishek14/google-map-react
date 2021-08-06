7fd5b29b007d65eb5408e1cd2d04727edaa4bf8d
Abhishek Kumar <hind.tigerabhisingh@gmail.com>
2021-08-06 23:38:37 +0530

    Google Map Added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
+ 
+
+// import React,{useEffect} from 'react';
+// import Geocode from "react-geocode";
+ 
+// const App = () => {
+//    useEffect(() => {
+//      console.log("Called")
+//     Geocode.setApiKey("AIzaSyCwi8jdM7JESQxOZVEsNaCQyeqK6iK1C4I");
+//     Geocode.enableDebug();
+  
+//     Geocode.fromLatLng("29.311661","47.481766" ).then(
+//       response => {
+//         const address = response.results[0].formatted_address;
+//         console.log("address",address)      
+//       },
+//       error => {
+//         console.error("error test",error);
+//       }
+//     );
+//   },[])
+//   return (
+//     <div>
+      
+//     </div>
+//   )
+// }
+ 
+// export default App
+
 import React from 'react'
 import Map from './Map/Map';
  
-
-
 const App = () => {
-
   const getData = (val) => {
-
     console.log("CHILD TO PARENT", val);
   }
-
-   
   return (
     <>
     <div>HELLO MAP </div>
---
+++ src/Map/Map1.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
+
+export class Map1 extends Component {
+  constructor(props) {
+    super(props);
+    this.onMarkerClick = this.onMarkerClick.bind(this);
+    this.state = {
+      showingInfoWindow: false,
+      activeMarker: {},
+      selectedPlace: {}
+    };
+  }
+  onMarkerClick(props, marker, e) {
+      console.log("props",props,e,marker);
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+  }
+  
+  
+  render() {
+    if (!this.props.google) {
+      return <div>Loading...</div>;
+    }
+
+    return (
+      <div
+        style={{
+          position: "relative",
+          height: "calc(100vh - 20px)"
+        }}
+      >
+        <Map style={{}} google={this.props.google} zoom={14} >
+          <Marker
+            onClick={this.onMarkerClick}
+            
+            name={"Current location"}
+          />
+          <InfoWindow
+            marker={this.state.activeMarker}
+            visible={this.state.showingInfoWindow}
+          >
+            <div>
+              <h1>{this.state.selectedPlace.name}</h1>
+            </div>
+          </InfoWindow>
+        </Map>
+      </div>
+    );
+  }
+}
+export default GoogleApiWrapper({
+  apiKey: "AIzaSyAyesbQMyKVVbBgKVi2g6VX7mop2z96jBo",
+  v: "3.30"
+})(Map1);
